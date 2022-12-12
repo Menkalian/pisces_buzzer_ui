b212ea2d2dcaa7f59f051c9dfa3f685e5a7e9ea5
Lars Rickert <dev@lars-rickert.de>
2022-12-12 22:58:28 +0100

    chore: add redirect to default buzzer

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import HomeView from "@/views/HomeView.vue";
+import config from "@/config";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
@@ -6,8 +6,7 @@ const router = createRouter({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      redirect: `/buzzer/${config.app.defaultBuzzerId}`,
     },
     {
       path: "/buzzer/:id",
